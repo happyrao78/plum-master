8a13a17b1e1a2ad387d7b257506d34041c70f176
HAPPY YADAV <happy.hy0017@gmail.com>
2024-09-21 13:20:04 +0530

    done all changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,12 +25,12 @@ export default function Navbar() {
                 {/* Menu Items */}
                 <div className={`flex-col md:flex-row md:flex md:space-x-4 items-center absolute md:static left-0 top-16 md:top-0 w-full md:w-auto bg-black md:bg-transparent p-4 md:p-0 transition-all duration-300 ease-in-out ${isOpen ? 'flex' : 'hidden'}`}>
                     <a href="#" className="text-[#FFEBDB] hover:text-[#FF4052]">Product</a>
-                    <a href="#" className="text-[#FFEBDB] hover:text-[#FF4052]">Experience</a>
+                    {/* <a href="#" className="text-[#FFEBDB] hover:text-[#FF4052]">Experience</a> */}
                     <a href="#" className="text-[#FFEBDB] hover:text-[#FF4052]">Explore</a>
-                    <a href="#" className="text-[#FFEBDB] hover:text-[#FF4052]">Careers</a>
+                    {/* <a href="#" className="text-[#FFEBDB] hover:text-[#FF4052]">Careers</a> */}
 
                     <button className="bg-transparent text-[#FFEBDB] border border-[#FF4052] py-2 px-4 rounded hover:bg-[#FF4052]">Login</button>
-                    <button className="bg-[#FF4052] text-white py-2 px-4 rounded hover:bg-transparent hover:border hover:border-[#FF4052]">Get a quote</button>
+                    {/* <button className="bg-[#FF4052] text-white py-2 px-4 rounded hover:bg-transparent hover:border hover:border-[#FF4052]">Get a quote</button> */}
                 </div>
             </div>
         </nav>
